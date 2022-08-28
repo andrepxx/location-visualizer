67478bbbf10c83ed1071b03419f867ceed872f71
Andre Plötze <7642349+andrepxx@users.noreply.github.com>
2022-08-28 14:31:45 +0200

    Add configuration option to disable TLS on the web server.
    Setting this option will switch to unencrypted HTTP communication.
    
    This option is provided to facilitate running the software behind a reverse
    proxy and should only be used in these scenarios.

--- controller/controller.go
+++ controller/controller.go
@@ -2237,8 +2237,16 @@ func (this *controllerStruct) runServer() {
 	} else {
 		requests := server.RegisterCgi("/cgi-bin/locviz")
 		server.Run()
-		tlsPort := serverCfg.TLSPort
-		fmt.Printf("Web interface ready: https://localhost:%s/\n", tlsPort)
+		protocol := "https"
+		port := serverCfg.TLSPort
+		tlsDisabled := serverCfg.TLSDisabled
+
+		if tlsDisabled {
+			protocol = "http"
+			port = serverCfg.Port
+		}
+
+		fmt.Printf("Web interface ready: %s://localhost:%s/\n", protocol, port)
 
 		/*
 		 * A worker processing HTTP requests.
